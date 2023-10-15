2fec20838afd595a8c32ed73829332b29fe0f02c
Pranav Shrikhande <pranav.shrikhande@gmail.com>
2023-10-15 12:35:36 +0530

    build navbar

---
+++ app/Navbar.tsx
@@ -0,0 +1,24 @@
+import Link from 'next/link'
+import React from 'react'
+import {AiFillBug} from 'react-icons/ai';
+
+const Navbar = () => {
+
+    const links = [
+        {label: 'Dashboard', href:'/'},
+        {label: 'Issues', href:'/issue'}
+    ]
+
+  return (
+    <nav className="flex space-x-6 border-b mb-5 px-5 h-14 items-center"><Link href="/"/><AiFillBug/>
+    <ul className='flex space-x-6'>
+        {links.map(link => <Link 
+        key= {link.href}
+        className='text-zinc-500 hover:text-zinc-800 transition-colors' href={link.href}>{link.label}</Link> )}
+    
+    </ul>
+    </nav>
+  )
+}
+
+export default Navbar
